d2649d45d6f374e2c694c017fb19aae9484e5d88
Henry Qi <henryqi222@gmail.com>
2017-07-06 19:39:41 -0700

    byte1

--- ByteShortInt/src/com/henry/Main.java
+++ ByteShortInt/src/com/henry/Main.java
@@ -3,6 +3,23 @@ package com.henry;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // int has a width of 32
+        int myMinValue = -2147483648;
+        int myMaxValue = 2147483647;
+        int myTotal = (myMinValue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        // byte has a width of 8
+        byte myByteValue = -128;
+        byte myNewByteValue = (byte) (myByteValue/2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
+
+        // short has a width of 16
+        short myShortValue = 32767;
+        short myNewShortValue = (short) (myShortValue/2);
+
+        // long has a width of 64
+        long myLongValue = 9223372036854775807L;
     }
 }
