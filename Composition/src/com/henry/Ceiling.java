a57f801c167f7361cb249ce95bf9fc7181c50b04
Henry Qi <henryqi222@gmail.com>
2017-07-20 22:09:53 -0700

    47

---
+++ Composition/src/com/henry/Ceiling.java
@@ -0,0 +1,22 @@
+package com.henry;
+
+/**
+ * Created by Henry on 7/20/2017.
+ */
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
