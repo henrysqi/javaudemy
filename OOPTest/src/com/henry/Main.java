d0f8485009647af23f3051742cd0825ecc7e7e3b
Henry Qi <henryqi222@gmail.com>
2017-07-21 22:29:17 -0700

    54

--- OOPTest/src/com/henry/Main.java
+++ OOPTest/src/com/henry/Main.java
@@ -3,6 +3,21 @@ package com.henry;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        price = hamburger.itemizeHamburger();
+        System.out.println("Total Burger price is " + price);
+
+        HealthyBurger healthyBurger  = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        db.itemizeHamburger();
     }
 }
